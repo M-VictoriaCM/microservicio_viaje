925567740338220685b4c74895848c93304c7387
M Victoria <mvittoria7@gmail.com>
2023-10-23 10:31:36 -0300

    creacion de la estructura del proyecto

---
+++ microservicio_viaje/src/main/java/com/microservicio_viaje/microservicio_viaje/MicroservicioViajeApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio_viaje.microservicio_viaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioViajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioViajeApplication.class, args);
+	}
+
+}
