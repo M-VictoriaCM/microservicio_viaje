7d78d4912e5142e24480adcff4141e369e1ebfd4
M Victoria <mvittoria7@gmail.com>
2023-11-01 00:52:02 -0300

    incorporacion de monopatines

---
+++ microservicio_viaje/src/main/java/com/microservicio_viaje/microservicio_viaje/model/Estado.java
@@ -0,0 +1,14 @@
+package com.microservicio_viaje.microservicio_viaje.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Estado {
+
+    private int idEstadoMonopatin;
+    private String estado;
+}
---
+++ microservicio_viaje/src/main/java/com/microservicio_viaje/microservicio_viaje/model/Monopatin.java
@@ -0,0 +1,16 @@
+package com.microservicio_viaje.microservicio_viaje.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Monopatin {
+    private int idMonopatin;
+    private int km_totales;
+    private String ubicacion;
+    private int km_recorridos;
+    private Estado estado;
+}
